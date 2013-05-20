26b17b7c6d7f9c16ad3cfb2424aea1727e2e04e3
Dmitry Vapelnik <dvapelnik@gmail.com>
2013-05-21 00:29:42 +0300

    Renaming Helper class to LocalStorageHelper Write carcase of some classes

--- notepad.js
+++ notepad.js
@@ -54,12 +54,24 @@ function Note() {
     }
 }
 
-function Helper(){
-    Helper.getFromStorageByKey = function(key){
+function LocalStorageHelper(){
+    LocalStorageHelper.getItemByKey = function(key){
+        if(LocalStorageHelper.checkStorage()){
+
+        }else{
+            throw new Error('Local storage is not available');
+        }
+    }
+
+    LocalStorageHelper.hasItemByKey = function(key){
 
     }
 
-    Helper.checkStorage = function(){
+    LocalStorageHelper.removeItemByKey = function(key){
+
+    }
+
+    LocalStorageHelper.checkStorage = function(){
         try {
             localStorage.setItem(mod, mod);
             localStorage.removeItem(mod);
