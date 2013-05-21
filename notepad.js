e67d128ab3669fc117430b10b7663358c4bf0012
Dmitry Vapelnik <dvapelnik@gmail.com>
2013-05-21 15:30:43 +0300

    Class Note and LocalStorageHelper moved into class Notepad inside

--- notepad.js
+++ notepad.js
@@ -9,75 +9,75 @@ function Notepad() {
     Notepad.addNote = function(note){
 
     }
-}
 
-function Note() {
-    this.title = undefined;
-    this.description = undefined;
-    this.date = undefined;
+    function Note() {
+        this.title = undefined;
+        this.description = undefined;
+        this.date = undefined;
 
-    Note.create = function (title, description, date) {
-        var note = new Note();
+        Note.create = function (title, description, date) {
+            var note = new Note();
 
-        note.title = title;
-        note.description = description;
-        note.date = date;
+            note.title = title;
+            note.description = description;
+            note.date = date;
 
-        return note;
-    }
+            return note;
+        }
 
-    Note.parseFromJSON = function (jsonString) {
-        var note = JSON.parse(jsonString, function (key, value) {
-            switch (key) {
-            case 'date':
-                return new Date(value);
-            default :
-                return value;
-            }
-        });
+        Note.parseFromJSON = function (jsonString) {
+            var note = JSON.parse(jsonString, function (key, value) {
+                switch (key) {
+                case 'date':
+                    return new Date(value);
+                default :
+                    return value;
+                }
+            });
 
-        return note;
-    }
+            return note;
+        }
 
-    this.toJSON = function () {
-        return JSON.stringify(this, function (key, value) {
-            switch (key) {
-            case 'title':
-            case 'description':
-                return value;
-            case 'date':
-                return value.toString();
-            default :
-                return undefined;
-            }
-        });
-    }
-}
-
-function LocalStorageHelper(){
-    LocalStorageHelper.getItemByKey = function(key){
-        if(LocalStorageHelper.checkStorage()){
-
-        }else{
-            throw new Error('Local storage is not available');
+        this.toJSON = function () {
+            return JSON.stringify(this, function (key, value) {
+                switch (key) {
+                case 'title':
+                case 'description':
+                    return value;
+                case 'date':
+                    return value.toString();
+                default :
+                    return undefined;
+                }
+            });
         }
     }
 
-    LocalStorageHelper.hasItemByKey = function(key){
+    function LocalStorageHelper(){
+        LocalStorageHelper.getItemByKey = function(key){
+            if(LocalStorageHelper.checkStorage()){
 
-    }
+            }else{
+                throw new Error('Local storage is not available');
+            }
+        }
 
-    LocalStorageHelper.removeItemByKey = function(key){
+        LocalStorageHelper.hasItemByKey = function(key){
 
-    }
+        }
 
-    LocalStorageHelper.checkStorage = function(){
-        try {
-            localStorage.setItem(mod, mod);
-            localStorage.removeItem(mod);
-            return true;
-        } catch(e) {
-            return false;
+        LocalStorageHelper.removeItemByKey = function(key){
+
+        }
+
+        LocalStorageHelper.checkStorage = function(){
+            try {
+                localStorage.setItem(mod, mod);
+                localStorage.removeItem(mod);
+                return true;
+            } catch(e) {
+                return false;
+            }
         }
     }
 }
